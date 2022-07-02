8eff69289b58d35c4d45b9e54ad507bae9351cff
Justin <justinmjames@protonmail.com>
2022-07-02 14:51:12 -0700

    fixed load/navigate but with games rendering on home-placed render code in fetch

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,36 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetchGames()
-   
-
+    fetchOnSaleGames()
     loadHome()
     homeLink()
     dealsByStoreLink()
 
 })
 
-/** Global Variables */
-let uniqueOnSaleGames = []
 
+/** Global Variables */
 
 
 /** Event Listeners */
+function homeLink() {
+    const homeLink = document.querySelector('.home-link')
+    homeLink.addEventListener('click', loadHome)
 
+}
 
+function dealsByStoreLink() {
+    const dealsByStoreLink = document.querySelector('.deals-by-store')
+    dealsByStoreLink.addEventListener('click', loadDealsByStore)
+}
 
 /**Node Getters */
 let main = () => document.getElementById('main')
 let storeDeals = () => document.getElementById('store-deals')
-let gameGrid = () => document.getElementById('game-grid')
+let homeGames = () => document.getElementById('home-games')
 
 
 /**Navbar Functions */
-
-// Resets all divs on navigation
+/**Resets all divs on navigation */
 const divReset = () => {
     main().innerHTML = ''
     storeDeals().innerHTML = ''
+    // homeGames().innerHTML = ''
 }
-
 
 // Home screen load 
 function loadHome() {
@@ -38,43 +42,10 @@ function loadHome() {
     let h1 = document.createElement('h1')
     h1.textContent = "Deals of the Day!"
     h1.className = 'center-align'
-    const ul = document.createElement('ul')
-    ul.id = 'game-grid'
     main().appendChild(h1)
-    main().appendChild(ul)
+    fetchOnSaleGames()
 
-    uniqueOnSaleGames.map(g => {
-        if (g.dealRating >= 9.8) {
-            const li = document.createElement('li')
-            li.className = 'game-container'
-            li.innerText = g.dealRating
-            let img = document.createElement('img')
-            img.className = 'homeGameImg'
-            img.src = g.thumb
-            ul.appendChild(li)
-            li.appendChild(img)
-
-        }
-
-
-
-
-    })
 }
-
-
-
-
-
-
-
-function homeLink() {
-    const homeLink = document.querySelector('.home-link')
-    homeLink.addEventListener('click', loadHome)
-}
-
-
-
 
 
 function loadDealsByStore() {
@@ -85,45 +56,53 @@ function loadDealsByStore() {
     storeDeals().appendChild(h1)
 }
 
-function dealsByStoreLink() {
-    const dealsByStoreLink = document.querySelector('.deals-by-store')
-    dealsByStoreLink.addEventListener('click', loadDealsByStore)
-}
-
-
-
-
 /**Fetch Functions */
+async function fetchOnSaleGames() {
+    const response = await fetch('https://www.cheapshark.com/api/1.0/deals?onSale')
+    const data = await response.json()
 
-function fetchGames() {
-    fetch('https://www.cheapshark.com/api/1.0/deals?onSale')
-        .then(res => res.json())
-        .then(res => {
-           
-           filterUniqueGames(res)
-        })
+    // filterUniqueGames(data) 
+    //value of the new Set() can only occur once will, delete repeats
+    const uniqueId = new Set()
+    const uniqueGames = data.filter(game => {
+        const isDuplicate = uniqueId.has(game.gameID)
+        uniqueId.add(game.gameID)
 
-}
+        if (!isDuplicate) {
+            return true
+        }
+        return false
+    });
 
+    const div = document.createElement('div')
+    div.id = 'home-games'
+    main().appendChild(div)
+    const ul = document.createElement('ul')
+    ul.id = 'game-grid'
+    div.appendChild(ul)
 
+    uniqueGames.map(g => {
+        if (g.dealRating >= 9.8) {
+            const li = document.createElement('li')
+            li.className = 'game-container'
+            li.innerText = `${g.title}`
+            const pSalePrice = document.createElement('p')
+            pSalePrice.innerText = `Sale Price ${g.salePrice}`
+            const spanNormalPrice = document.createElement('span')
+            spanNormalPrice.innerText = `Normal Price ${g.normalPrice}`
+            const img = document.createElement('img')
+            img.className = 'homeGameImg'
+            img.src = g.thumb
+            ul.appendChild(li)
+            li.appendChild(img)
+            li.appendChild(spanNormalPrice)
+            li.appendChild(pSalePrice)
 
+        }
 
-/**Fetch manipulation */
-function filterUniqueGames(res){
-    
-    const uniqueId = new Set() 
-    const unique = res.filter(game => {
-     const isDuplicate = uniqueId.has(game.gameID)
-     uniqueId.add(game.gameID)
-
-     if(!isDuplicate){
-         return true
-     }
-     return false
     })
- uniqueOnSaleGames = unique
+
+
 }
 
-
-
-/**Data to DOM Functions */
+/**Fetch data manipulation */
