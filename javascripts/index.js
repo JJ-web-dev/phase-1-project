b8515fbf49387dca8dde5b0e93aa06440249dce5
Justin <justinmjames@protonmail.com>
2022-07-07 16:57:04 -0700

    completed get request to json function, games now save to serve and add to dom on wishlist load

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     loadHome()
     homeLink()
     dealsByStoreLink()
-    setTimeout(postWishlistGame, 4000)
-    // mousePointerChange()
+    wishlistLink()
+    // setTimeout(postWishlistGame, 2000)
+    wishlistGamesToDOM()
+
+
+
+
+
 
 })
 
@@ -21,6 +27,11 @@ function homeLink() {
 function dealsByStoreLink() {
     const dealsByStoreLink = document.querySelector('.deals-by-store')
     dealsByStoreLink.addEventListener('click', loadDealsByStore)
+}
+
+function wishlistLink() {
+    const wishlistLink = document.querySelector('.wishlist')
+    wishlistLink.addEventListener('click', loadWishList)
 }
 
 function mousePointerChange() {
@@ -44,7 +55,7 @@ let homeGames = () => document.getElementById('home-games')
 const divReset = () => {
     main().innerHTML = ''
     storeDeals().innerHTML = ''
-    // homeGames().innerHTML = ''
+    wishlist().innerHTML = ''
 }
 
 // Home screen load 
@@ -54,9 +65,8 @@ function loadHome() {
     h1.textContent = "Deals of the Day!"
     h1.className = 'center-align'
     main().appendChild(h1)
-    // filterUniqueGames()
     fetchOnSaleGames()
-
+    setTimeout(postWishlistGame, 2000)
 }
 
 
@@ -68,13 +78,15 @@ function loadDealsByStore() {
     storeDeals().appendChild(h1)
 }
 
-// function loadWishList(){
-//     divReset()
-//     let h1 = document.createElement('h1')
-//     h1.textContent = 'WishList'
-//     wishlist().appendChild(h1)
-    
-// }
+function loadWishList() {
+    divReset()
+    let h1 = document.createElement('h1')
+    h1.textContent = 'WishList'
+    h1.className = 'center-align'
+    wishlist().appendChild(h1)
+    wishlistGamesToDOM()
+
+}
 
 
 
@@ -84,16 +96,87 @@ async function fetchOnSaleGames() {
     const data = await response.json()
 
     const games = await filterUniqueGames(data)
-    gameOnDom(games)
-    // postWishlistGame() 
-    // return data
+
+    const filterGames = filterGameDeals(games)
+
+    gameOnDom(filterGames)
 
 }
 
+function filterGameDeals(game) {
+    let gameDeal = []
+    game.map(games => {
+        if (games.dealRating >= 9.6) {
+
+            gameDeal.push(games)
+        }
+    })
+    return gameDeal
+}
+
+//Need to add alert once successful post is made
+function postWishlistGame() {
+
+    const addWishListEvent = document.querySelectorAll('.add-to-wishlist')
+
+    addWishListEvent.forEach(item => item.addEventListener('click', e => {
+
+        const gameID = e.target.id
+          console.log(gameID)
+
+        fetch(`https://www.cheapshark.com/api/1.0/games?id=${gameID}`)
+            .then(response => response.json())
+            .then(response => {
+                const gameInfo = response.info
+
+
+                fetch('http://localhost:3000/posts', {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json"
+                    },
+                    body: JSON.stringify(gameInfo)
+                })
+
+            })
+    }))
+}
+//Loads games that were added to json wishlist on wishlist page
+async function wishlistGamesToDOM(game) {
+    const response = await fetch('http://localhost:3000/posts')
+    const wishlistData = await response.json()
+    
+
+    const div = document.createElement('div')
+    div.id = 'wishlist-games'
+    wishlist().appendChild(div)
+    const ul = document.createElement('ul')
+    ul.id = 'game-grid'
+    div.appendChild(ul)
+
+    const [firstElement, ...restArray] = wishlistData 
+    
+    restArray.map(g => {
+
+        const li = document.createElement('li')
+        li.className = 'game-container'
+        li.innerText = `${g.title}`
+        const img = document.createElement('img')
+        img.className = 'wishlistGameImg'
+        img.src = g.thumb
+        ul.appendChild(li)
+        li.appendChild(img)
+
+
+    })
+
+
+}
+
+
 // filters the onSaleGames so no repeat games are rendered
 function filterUniqueGames(data) {
-    // const data = await fetchOnSaleGames()
-
     const uniqueId = new Set()
     const uniqueGames = data.filter(game => {
         const isDuplicate = uniqueId.has(game.gameID)
@@ -107,16 +190,6 @@ function filterUniqueGames(data) {
 
     return uniqueGames
 
-    // gameOnDom(uniqueGames)
-    
-    // postWishlistGame()
-
-
-}
-
-async function games(){
-    const data = await filterUniqueGames()
-    gameOnDom(data)
 }
 
 //adds games to the homepage DOM
@@ -130,82 +203,24 @@ function gameOnDom(uniqueGames) {
     div.appendChild(ul)
 
     uniqueGames.map(g => {
-        if (g.dealRating >= 9.8) {
-            const li = document.createElement('li')
-            li.className = 'game-container'
-            li.innerText = `${g.title}`
-            const spanSalePrice = document.createElement('span')
-            spanSalePrice.innerText = `Sale Price ${g.salePrice}`
-            const spanNormalPrice = document.createElement('span')
-            spanNormalPrice.innerText = `Normal Price ${g.normalPrice}`
-            const img = document.createElement('img')
-            img.className = 'homeGameImg'
-            img.src = g.thumb
-            const addWishlist = document.createElement('span')
-            addWishlist.className = 'add-to-wishlist'
-            addWishlist.id = g.gameID
-            addWishlist.innerText = 'Add to Wishlist'
-            ul.appendChild(li)
-            li.append(img, spanNormalPrice, spanSalePrice, addWishlist)
+
+        const li = document.createElement('li')
+        li.className = 'game-container'
+        li.innerText = `${g.title}`
+        const spanSalePrice = document.createElement('span')
+        spanSalePrice.innerText = `Sale Price ${g.salePrice}`
+        const spanNormalPrice = document.createElement('span')
+        spanNormalPrice.innerText = `Normal Price ${g.normalPrice}`
+        const img = document.createElement('img')
+        img.className = 'homeGameImg'
+        img.src = g.thumb
+        const addWishlist = document.createElement('span')
+        addWishlist.className = 'add-to-wishlist'
+        addWishlist.id = g.gameID
+        addWishlist.innerText = 'Add to Wishlist'
+        ul.appendChild(li)
+        li.append(img, spanNormalPrice, spanSalePrice, addWishlist)
 
 
-        }
     })
 }
-
-//Need to add alert once successful post is made
-function postWishlistGame() {
-
-    const addWishListEvent = document.querySelectorAll('.add-to-wishlist')
-    
-    addWishListEvent.forEach(item => item.addEventListener('click', e => {
-                
-               const gameID = e.target.id
-              
-        
-        fetch(`https://www.cheapshark.com/api/1.0/games?id=${gameID}`)
-            .then(response => response.json())
-            .then(response => {
-                const game = response
-                console.log(game)
-
-                fetch('http://localhost:3000/posts', {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "application/json"
-                    },
-                    body: JSON.stringify(game)
-
-
-                })
-            })
-    }))
-}
-
-
-
-//     e.preventDefault()
-//     console.log(addWishListEvent)
-//     const [name, image] = e.target
-
-//     fetch('http://localhost:3000/wishList', {
-//         method: 'POST',
-//         headers: {
-//             "Content-Type": "application/json",
-//             Accept: "application/json"
-//         },
-//         body: JSON.stringify({
-//             name: title.value,
-//             image: thumb.value
-//         })
-//     })
-// })
-// .then((res => res.json))
-// .then(res => {
-//     console.log(res)
-// })
-
-
-
-/**Fetch data manipulation */
